f51e029dc496bd36b5c7741c2db32c959b4bb18f
Ablanya Charles feranmi <charlesablanya@gmail.com>
2025-04-18 13:30:15 +0100

    modified:   client/src/App.jsx #    new file:   client/src/component/seller/SellerLogin.jsx #    modified:   client/src/index.css #    new file:   client/src/pages/seller/AddProduct.jsx #    new file:   client/src/pages/seller/Orders.jsx #    new file:   client/src/pages/seller/ProductList.jsx #    new file:   client/src/pages/seller/SellerLayout.jsx #    new file:   package-lock.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,17 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
+import SellerLogin from './component/seller/SellerLogin';
+import SellerLayout from './pages/seller/SellerLayout';
+import AddProduct from './pages/seller/AddProduct';
+import ProductList from './pages/seller/ProductList';
+import Orders from './pages/seller/Orders';
 
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLgin} = useAppContext()
+  const {showUserLgin , isSeller} = useAppContext()
   return (
-    <div>
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
       {/* Conditionally render Navbar only if not a "seller" path */}
       {!isSellerPath && <Navbar />}
       {showUserLgin && <Login />}
@@ -33,6 +38,12 @@ function App() {
           <Route path='/Cart' element={<Cart/>} />
           <Route path='/add-address' element={<AddAddress/>} />
           <Route path='/my-orders' element={<MyOrders/>} />
+         <Route path='/seller' element={isSeller ? <SellerLayout/>  : <SellerLogin/>}>
+<Route index element={isSeller ? <AddProduct/> : null}/>
+<Route path='product-list' element={<ProductList/>} />
+<Route path='orders' element={<Orders/>} />
+
+         </Route>
         </Routes>
       </div>
      {!isSellerPath && <Footer/>} 
